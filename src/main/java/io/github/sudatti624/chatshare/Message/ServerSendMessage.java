974a160a480771a23a92db29a1877b30fb8e463e
sudatti624 <165920415+sudatti624@users.noreply.github.com>
2026-03-17 14:55:07 +0900

    リファクタリング: ServerSendMessage を新しい sendMessage API を使用するように更新
    - sendToDiscord() → sendMessage() に変更
    - クラスドキュメントを追加

--- src/main/java/io/github/sudatti624/chatshare/Message/ServerSendMessage.java
+++ src/main/java/io/github/sudatti624/chatshare/Message/ServerSendMessage.java
@@ -2,16 +2,17 @@ package io.github.sudatti624.chatshare.Message;
 
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 
-import static io.github.sudatti624.chatshare.Message.SendToDiscord.sendToDiscord;
+import static io.github.sudatti624.chatshare.Sender.SendToDiscord.sendMessage;
 
+/**
+ * Minecraftのチャットメッセージを受信し、Discordへ転送する。
+ */
 public class ServerSendMessage {
     public void register() {
         ServerMessageEvents.CHAT_MESSAGE.register((message, sender, server) -> {
+            String playerName  = sender.getName().getString();
             String chatContent = message.getContent().getString();
-            String playerName = sender.getName().getString();
-            String messageContent = "<" + playerName + ">: " + chatContent;
-
-            sendToDiscord(messageContent);
+            sendMessage("<" + playerName + "> " + chatContent);
         });
     }
 }
